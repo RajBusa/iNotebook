e7503c8d8d11a2afdf148059dbe4f8f8263dc512
RajBusa <20it0112charusat.edu.in>
2022-07-09 10:45:34 +0530

    video 47 completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,12 +2,14 @@ const express = require('express')
 const router = express.Router();
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
+const { ResultWithContext } = require('express-validator/src/chain');
 
-router.post('/', [
+
+router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must have 5 character').isLength({ min: 5 }),
-] ,(req, res)=>{
+] , async (req, res)=>{
     // obj = {
     //     name: "Raj",
     //     number: 458
@@ -17,20 +19,32 @@ router.post('/', [
     // const user = User(req.body)
     // user.save();
 
+    // If there are error, return bad request and the error 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-      }).then(user => res.json(user))
-      .catch (err =>{ console.log(err)
-      res.json({error: 'Please enter a unique value for email', message: err.message})})
-
-    // res.send(req.body)
+    // 
+    try {
+        // Check wether the user with this email exist already
+        let user = await User.findOne({email: req.body.email})
+    
+        if(user){
+            return res.status(400).json({message: "Sorry a user with this email already exist"})
+        }
+    
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.send(user)
+    } catch (error) {
+        console.log(error.massage)
+        res.status(500).json({error: "some error exists"})
+    }
+    
 });
 
 module.exports = router
