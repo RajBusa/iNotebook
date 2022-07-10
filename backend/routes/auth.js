20f680f49c5a3e25445fa8f15c379844f347ed39
RajBusa <20it0112charusat.edu.in>
2022-07-10 12:34:17 +0530

    video 49 completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router();
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
-const { ResultWithContext } = require('express-validator/src/chain');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "Jay Swaminarayan"
+// const { ResultWithContext } = require('express-validator/src/chain');
 
 
 router.post('/createuser', [
@@ -34,14 +38,27 @@ router.post('/createuser', [
             return res.status(400).json({message: "Sorry a user with this email already exist"})
         }
     
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+
+
         user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: secPass,
         })
-        res.send(user)
+
+        const data = {
+            user:{
+                id:user.id
+            }
+        }
+        const jwtData = jwt.sign(data, JWT_SECRET)
+        console.log(jwtData)
+        res.json({jwtData})
+        // res.send(user)
     } catch (error) {
-        console.log(error.massage)
+        console.log(error.message)
         res.status(500).json({error: "some error exists"})
     }
     
